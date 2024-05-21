e8672ace2a7aeede3372dd7404b418b56237d216
MarinaBarrov <maari-b@live.com.ar>
2024-05-20 22:56:17 -0300

    Mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Card } from './componentes/Card'
+import { Form } from './componentes/Form'
+
+function App() {
+
+
+  return (
+    <>
+    <Form/>
+    <Card/>
+    </>
+  )
+}
+
+export default App
